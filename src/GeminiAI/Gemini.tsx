68deebbf25b0f8b787f386ce61c53bf569908d87
Frank <temitopeadeoye787898@gmail.com>
2025-08-21 01:41:11 +0100

    Completed the HTML for the gemini project

--- src/GeminiAI/Gemini.tsx
+++ src/GeminiAI/Gemini.tsx
@@ -2,49 +2,59 @@
 
 function Gemini() {
   return (
-    <div className="max-w-[1000px] py-[70px] mx-auto">
+    <div className="container max-w-[1000px] py-[70px] mx-auto">
+      {/* App Header */}
       <header className="app-header">
         <h1 className="heading text-3xl font-semibold">Hello, there</h1>
         <h2 className="sub-heading text-3xl font-semibold">How can I help you</h2>
       </header>
 
-      <div className="flex gap-3.5 mt-[87px]">
-        <div>
-          <p>Design a home office setup for remote work under $500</p>
+      {/* Suggestions List */}
+      <ul className="suggestions flex gap-3.5 mt-[87px]">
+        <li className="suggestions_item">
+          <p className="text">Design a home office setup for remote work under $500</p>
+
+          <span className="material-symbols-rounded">draw</span>
+        </li>
+
+        <li className="suggestions_item">
+          <p className="text">Design a home office setup for remote work under $500</p>
+
+          <span className="material-symbols-rounded">lightbulb</span>
+        </li>
+
+        <li className="suggestions_item">
+          <p className="text">Design a home office setup for remote work under $500</p>
 
           <button type="button"><img src="" alt="" /></button>
-        </div>
+        </li>
 
-        <div>
-          <p>Design a home office setup for remote work under $500</p>
-
-          <button type="button"><img src="" alt="" /></button>
-        </div>
-
-        <div>
-          <p>Design a home office setup for remote work under $500</p>
+        <li className="suggestions_item">
+          <p className="text">Design a home office setup for remote work under $500</p>
 
           <button type="button"><img src="" alt="" /></button>
+        </li>
+      </ul>
+
+      {/* Prompt Container */}
+      <div className="prompt-container">
+        <div className="prompt-wrapper">
+          <form className="prompt_form flex">
+            <div className="flex-1 flex">
+              <input type="text" className="prompt_input flex-1"/>
+
+              <div className="prompt_actions">
+                <button className="w-[40px] h-[40px]"><span className="material-symbols-rounded">attach_file</span></button>
+                <button><span className="material-symbols-rounded">arrow_upward_alt</span></button>
+              </div>
+            </div>
+
+            <button className="material-symbols-rounded">light_mode</button>
+            <button className="material-symbols-rounded">delete</button>
+          </form>
         </div>
 
-        <div>
-          <p>Design a home office setup for remote work under $500</p>
-
-          <button type="button"><img src="" alt="" /></button>
-        </div>
-      </div>
-
-      <div>
-        <form className="flex">
-          <div className="flex-1 flex">
-            <input type="text" className="flex-1"/>
-            <button className="w-[40px] h-[40px]"><span className="material-symbols-outlined">attach_file</span></button>
-            <button><span className="material-symbols-outlined">arrow_upward_alt</span></button>
-          </div>
-
-          <button><span className="material-symbols-outlined">light_mode</span></button>
-          <button><span className="material-symbols-outlined">delete</span></button>
-        </form>
+        <p className="disclaimer-text">Gemini can make mistakes, so double-check it.</p>
       </div>
     </div>
   )
